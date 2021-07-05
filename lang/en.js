6d4393ea876c41c2e627d47205b1a731bc1a8e7a
szobi <ps@gamerhash.com>
2021-07-05 14:13:51 +0200

    translate en

--- lang/en.js
+++ lang/en.js
@@ -1,19 +1,80 @@
 export default {
-    heroTitle:'SOFTWARE HOUSE',
-    heroSlogan:'We provide applications and software for start-ups and corporate clients using the latest technologies and solutions.',
-    more:'more',
-    contact:'contect',
-    navbar:{
-        home:'home',
-        service:'services',
-        howWork:'how do we work',
-        about:'about us',
-        contact:'contact'
-    },
-    services:'services',
-    servicesDescription:'Meet the wide range of our services together with professional approach that will help you realize your business goals using the latest technology and solutions.    ',
-    servicesItems:{
-        softwareDevelopment:'Software development',
-        softwareDevelopmentDescription:'Use many years of our experience in the area of software development for enterprises/businesses, system modernization and consolidation of appliaction wallet. We will help you achieve sustained success through redesigning solutions of different complexity.',
-    }
+  seo: {
+    title: 'CoinAxe – Tworzymy i rozwijamy oprogramowanie i aplikacje',
+    description:
+      'Dostarczanie aplikacji i oprogramowania dla start-upów i klientów korporacyjnych, poprzez wykorzystanie najnowszych technologii i rozwiązań.',
+  },
+  heroTitle: 'SOFTWARE HOUSE',
+  heroSlogan:
+    'We provide applications and software for start-ups and corporate clients using the latest technologies and solutions.',
+  more: 'more',
+  contact: 'contect',
+  navbar: {
+    home: 'home',
+    service: 'services',
+    howWork: 'how do we work',
+    about: 'about us',
+    contact: 'contact',
+  },
+  services: 'services',
+  servicesDescription:
+    'Meet the wide range of our services together with professional approach that will help you realize your business goals using the latest technology and solutions.    ',
+  servicesItems: {
+    softwareDevelopment: 'Software development',
+    softwareDevelopmentDescription:
+      'Use many years of our experience in the area of software development for enterprises/businesses, system modernization and consolidation of appliaction wallet. We will help you achieve sustained success through redesigning solutions of different complexity.',
+    mobileApps: 'Tworzenie aplikacji mobilnych',
+    mobileAppsDescription:
+      'Oferujemy pełen cykl usług projektowania, integracji i zarządzania aplikacjami. Niezależnie od tego, czy jest to aplikacja zorientowana na konsumenta, czy rozwiązanie klasy korporacyjnej. Prowadzimy cały proces tworzenia aplikacji mobilnej od pomysłu i koncepcji po dostarczenie i stałe wsparcie.',
+    itCoach: 'IT consulting',
+    itCoachDescription:
+      'Benefit from the advanced technology and knowledge of our best CoinAxe’s employees, to create a complex IT strategy concerning digital and technological transformation of your organisation, which will be compatible with your business goals.',
+    uiux: 'UX / UI design',
+    uiuxDescription:
+      'Do you want to build your product with a team that will establish a clear design process, meet deadlines and assure an excellent final result? Our project team will help you build an interesting solution fast and with ease.',
+    testing: 'Testing and quality control',
+    testingDescription:
+      'Our services will allow you to monitor every step of development of the app. Moreover, you will be provided with the detailed information about the quality of the product. Our team is equipped with all the skills and experience necessary to test the product on every step of the building process.',
+    team: 'Development teams',
+    teamDescription:
+      'Calibrate the efficiency of the deliveries through intelligent and dedicated programmers. Our clients succeed when they work with our team on the design, building, testing, implementation and maintenance processes.',
+  },
+  howWork: 'HOW DO WE WORK',
+  howWorkItems: {
+    analytics: 'ANALYSIS',
+    analyticsDescription:
+      'On that level, we define the right tools, libraries and set a proper plan of development in the process of realization of an ordered solution.',
+    project: 'DESIGN',
+    projectDescription:
+      'Primarily it is a preparation of a solution’s sculpture and it’s design and functionality aspects.Primarily it is a preparation of a solution’s sculpture and it’s design and functionality aspects. ',
+    production: 'DEVELOPMENT',
+    productionDescription:
+      'Our teams put an effort into the project to make the final product meet your expectations and the highest standards.',
+    implementation: 'IMPLEMENTATION',
+    implementationDescription:
+      'The final step includes delivering a finished product, administrators’ training and a constant technical support.',
+  },
+  whoWeAre: 'WHO ARE WE?',
+  whoWeAreDescription:
+    'We are a company dealing with the consultancy in terms of the digital transformation and developing software. We will provide the latest business solutions for your firm. We help out start-ups and corporate clients with solving complex issues, which always appear on their digital evolution.',
+  whoWeAreItems: {
+    knowledge: 'KNOWLEDGE',
+    knowledgeDescription:
+      'Our specialists guarantee the solutions based on the latest technologies. Years of experience and directional education enables us to achieve the designated goals and accomplish the impossible.',
+    expirience: 'EXPERIENCE',
+    expirienceDescription:
+      'Experience gained from numerous projects of our developers, testers and designers allows us to deliver products of the best quality.      ',
+    team: 'team',
+    teamDescription:
+      'Amazing atmosphere, great people and a lot of positive energy... this is what characterizes our team. Together we can move mountains:)',
+  },
+  skills: 'OUR SKILLS',
+  progressProject: 'DESIGN',
+  progressUxui: 'ux/ui',
+  progressProgramming: 'development',
+  progressTesting: 'testing',
+  contactWithUs: 'contact us',
+  contactWithUsDescription:
+    'Send us a message. We are waiting for your message! We will answer all your questions. office[at]coinaxe.eu    ',
+  office: 'Office in Malta',
 }
